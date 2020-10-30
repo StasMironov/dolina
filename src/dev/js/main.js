6a4926cf90a68f4574c9aeccada144ce73129867
MironovS <stasmironovweb@gmail.com>
2020-10-30 16:53:55 +0300

    fix

--- src/dev/js/main.js
+++ src/dev/js/main.js
@@ -113,17 +113,135 @@ if ($('#map').exists()) {
 let tabsInfo = document.querySelectorAll('.tab__info');
 let tabsContent = document.querySelectorAll('.tab__content');
 
-console.log(tabsContent);
+const timeline = gsap.timeline();
 
-for (let i = 0; i < tabsInfo.length; i++) {
-    tabsInfo[i].onclick = function () {
-        for (let j = 0; j < tabsContent.length; j++) {
-            if (j == i) {
-                console.log('Tabs');
+
+let tab = document.querySelectorAll('.tab__info'),
+    info = document.querySelector('.tab__header'),
+    tabContent = document.querySelectorAll('.tab__content');
+
+tab[0].classList.add('tab__info--active');
+
+for (let i = 0; i < tabContent.length; i++) {
+
+    if (i != 0) {
+        gsap.set(tabContent[i], {
+            autoAlpha: 0,
+            //display: 'none',
+        });
+    }
+
+}
+
+
+function hideTabContent(a) {
+    for (let i = a; i < tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add('hide');
+        tab[i].classList.remove('tab__info--active');
+
+
+        timeline
+            .fromTo(
+                tabContent[i],
+                {
+                    autoAlpha: 1,
+                    // display: 'bloc',
+                    xPercent: 0,
+                },
+                {
+                    autoAlpha: 0,
+                    //display: 'none',
+                    ease: 'power2.out',
+                    xPercent: +100,
+                }
+            )
+    }
+}
+
+hideTabContent(1);
+
+function showTabContent(b) {
+    if (tabContent[b].classList.contains('hide')) {
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+        tab[b].classList.add('tab__info--active');
+        timeline
+            .fromTo(
+                tabContent[b],
+                {
+                    autoAlpha: 0,
+                    // display: 'none',
+                    xPercent: +100,
+                },
+                {
+                    autoAlpha: 1,
+                    // display: 'block',
+                    xPercent: 0,
+                    duration: 1,
+                    ease: "back"
+                }
+            )
+    }
+
+
+}
+
+info.addEventListener('click', function (event) {
+
+    let target = event.target;
+    if (target && target.classList.contains('tab__info')) {
+        for (let i = 0; i < tab.length; i++) {
+            tab[i].classList.remove('tab__info--active');
+            if (target == tab[i]) {
+                hideTabContent(0);
+                showTabContent(i);
+                break;
             }
         }
     }
-}
+});
+
+
+
+
+// for (let i = 0; i < tabsInfo.length; i++) {
+//     gsap.set(
+//         tabsContent[0],
+//         {
+//             xPercent: 0,
+//             opacity: 1
+//         }
+//     );
+
+//     gsap.set(
+//         tabsContent[1],
+//         {
+//             xPercent: +100,
+//             opacity: 0
+//         }
+//     );
+
+//     tabsInfo[i].onclick = function () {
+//         for (let j = 0; j < tabsContent.length; j++) {
+//             if (j == i) {
+//                 console.log(tabsContent[j]);
+//                 timeline
+//                     .from(
+//                         tabsContent[j],
+//                         {
+//                             xPercent: 0,
+//                             opacity: 1,
+//                             duration: 1,
+//                             ease: 'bounce'
+//                         }
+//                     );
+//                 // timeline.play();
+//             }
+
+//         }
+//     }
+// }
 
 
 
